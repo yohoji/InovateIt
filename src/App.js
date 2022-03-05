891cba90e39d61a0e89e0298b907385263fa5e16
kamelht <Kamel.htira@isamm.u-manouba.tn>
2022-03-05 17:00:50 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ResponsiveAppBar from './component/navbar'
 import HomeContainer from './component/homeContainer'
 import { Modal } from './component/Modal'
 import {useState} from 'react'
+import {Routes,Route} from 'react-router-dom'
 
 
 
@@ -15,6 +16,19 @@ function App() {
     <div className="App">
      
       <ResponsiveAppBar/>
+      <Routes>
+        <Route path='/' exact>
+
+        </Route>
+
+        <Route path='/courses' >
+            
+        </Route>
+
+        <Route path='/contact' >
+            
+        </Route>
+      </Routes>
        <HomeContainer/>
     </div>
   );
